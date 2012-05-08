df5e8e5b2984c5ec1c58aca47a9488f5ef3ab5cd
Saidul Islam <saidul.04@gmail.com>
2012-05-08 23:53:37 +0600

    Indentation fix

--- Response/XsendfileResponseFactory.php
+++ Response/XsendfileResponseFactory.php
@@ -9,8 +9,6 @@ class XsendfileResponseFactory extends AbstractResponseFactory
     protected function setResponseHeaders(Response $response)
     {
         parent::setResponseHeaders($response);
-
-        // $response->headers->set('X-Sendfile', realpath($this->fullFilename));
         $response->headers->set('X-Sendfile', $this->fullFilename);
     }
 }
